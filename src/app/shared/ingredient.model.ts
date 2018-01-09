e8c67552958a919ef2fa39e74c08e80cfe5a63a1
Paulo Moggi <paulohpmoggi@gmail.com>
2018-01-09 12:17:27 +0000

    Add components UIs

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,8 @@
+export class Ingredient {
+    // public name: string;
+    // public amount: number;
+    // Those public properties are defined on the constructor method
+
+    constructor(public name: string, public amount: number) {}
+
+}
